43a84653c548768a22acbff902afb98d379939c5
DevCactusTecnologia <136168608+DevCactusTecnologia@users.noreply.github.com>
2026-09-06 23:44:44 -0300

    fix: preserva rota raiz ao adicionar autenticação

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\SessionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn () => view('welcome'));
+
 Route::prefix('api/auth')->group(function () {
     Route::post('/login', LoginController::class)->name('auth.login');
 
