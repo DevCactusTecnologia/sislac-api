6a7aecd514b788a17fd27418a092bef71593c230
DevCactusTecnologia <136168608+DevCactusTecnologia@users.noreply.github.com>
2026-09-07 00:51:35 -0300

    fix: ativa provider de tenancy

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\PlatformServiceProvider;
+use App\Providers\TenancyServiceProvider;
 
 return [
     AppServiceProvider::class,
     PlatformServiceProvider::class,
+    TenancyServiceProvider::class,
 ];
