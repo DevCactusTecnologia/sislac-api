41633ae3b67e41c5b8cfd689a704b3f41564169b
DevCactusTecnologia <136168608+DevCactusTecnologia@users.noreply.github.com>
2026-09-07 19:44:36 -0300

    feat: add Super Admin login view

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layout')
+
+@section('title', 'Acesso administrativo')
+
+@section('content')
+    <h1>Acesso administrativo</h1>
+    <p>Entre com sua conta central do SISLAC.</p>
+@endsection
