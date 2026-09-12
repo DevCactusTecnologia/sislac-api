1d232554189a2c25d9e1ef3e0ce719d83d768363
DevCactusTecnologia <136168608+DevCactusTecnologia@users.noreply.github.com>
2026-09-12 11:12:57 -0300

    feat: habilita sanctum stateful para spa

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->statefulApi();
         $middleware->trustProxies(at: [
             '127.0.0.1',
             '10.0.0.0/8',
